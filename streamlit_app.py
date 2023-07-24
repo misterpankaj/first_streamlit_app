60a6b0a0b2ef068f097e13c3545e38f1c79bfdf3
TechMindForYou <36676550+misterpankaj@users.noreply.github.com>
2023-07-24 14:59:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('My Parents New Healthy Diner')
+
+streamlit.text('My Parents New Healthy Diner')
+streamlit.text('My Parents New Healthy Diner')
+streamlit.text('My Parents New Healthy Diner')
